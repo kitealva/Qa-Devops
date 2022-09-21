4d6303ded8e9432508f20225f73cebfc8fe976cc
Brady <kite.alva@gmail.com>
2022-09-21 15:02:36 -0600

    firstcommit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+// app.use('/', express.static(path.join(__dirname,'../client/index.html')))
+
+// app.use(express.static(path.join(__dirname, '../client/main.js')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'../client/index.html'))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname,'../client/main.js'))
+})
+
+
+
+
+app.listen(port, () => {
+    console.log('Docked at port' + port)
+})
